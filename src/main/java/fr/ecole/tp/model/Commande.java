560d4d7cec38e34d5b79f136392ebd88830a3b57
Teddy <teddynsoki@gmail.com>
2026-05-29 11:57:19 +0200

    remove final from class attributes

--- src/main/java/fr/ecole/tp/model/Commande.java
+++ src/main/java/fr/ecole/tp/model/Commande.java
@@ -3,9 +3,9 @@ package fr.ecole.tp.model;
 import java.util.List;
 
 public class Commande {
-    private final String id;
-    private final Client client;
-    private final List<LigneCommande> lignes;
+    private String id;
+    private Client client;
+    private List<LigneCommande> lignes;
 
     public Commande(String id, Client client, List<LigneCommande> lignes) {
         this.id = id;
